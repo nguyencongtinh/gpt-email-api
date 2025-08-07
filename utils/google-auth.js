e17cea5008e3a8a83bc48b47b62ece48a22c9e0d
HamHocHoi <nguyencongtinh@gmail.com>
2025-08-07 12:46:25 +0700

    Update google-auth.js

--- utils/google-auth.js
+++ utils/google-auth.js
@@ -1,1 +1,7 @@
+// File giả lập – Anh cần thay bằng logic thật lấy dữ liệu từ Google Sheets và gửi Gmail
+
+export async function sendReminderEmails() {
+  console.log("Simulating sending reminder emails...");
+  // TODO: Connect to Google Sheets API, check expiry, send email via Gmail API
+}
 
