4d10c02dc50b246f332d8277102c8c26faa86dad
HamHocHoi <nguyencongtinh@gmail.com>
2025-08-07 14:50:36 +0700

    Update send-reminder.js

--- api/send-reminder.js
+++ api/send-reminder.js
@@ -2,10 +2,12 @@ import { sendReminderEmails } from '../utils/google-auth';
 
 export default async function handler(req, res) {
   try {
+    console.log("🔔 Đang gọi sendReminderEmails...");
     await sendReminderEmails();
+    console.log("✅ Đã gửi email thành công!");
     res.status(200).send("Reminder emails sent successfully.");
   } catch (err) {
-    console.error(err);
+    console.error("❌ Lỗi khi gửi email: ", err);
     res.status(500).send("Failed to send reminder emails.");
   }
 }
