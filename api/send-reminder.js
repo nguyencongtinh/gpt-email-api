e7e7c4533c5514f704285bbf3abe299d1a112697
HamHocHoi <nguyencongtinh@gmail.com>
2025-08-08 10:21:55 +0700

    Add files via upload

--- api/send-reminder.js
+++ api/send-reminder.js
@@ -1,10 +1,10 @@
 const { sendReminderEmails } = require("../utils/send-reminder-logic");
 
-module.exports = async function handler(req, res) {
-  console.log("🔔 Đang gọi sendReminderEmails...");
+module.exports = async (req, res) => {
   try {
+    console.log("🔔 Đang gọi sendReminderEmails...");
     await sendReminderEmails();
-    res.status(200).send("✅ Đã gửi email nhắc hạn (nếu có).");
+    res.status(200).send("✅ Đã gửi xong nhắc hạn.");
   } catch (error) {
     console.error("❌ Lỗi khi gửi email: ", error);
     res.status(500).send("❌ Lỗi khi gửi email.");
